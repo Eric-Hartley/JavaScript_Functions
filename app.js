d688af541763885dce5517c1472231999047ea56
Eric Hartley <spiffydude0@gmail.com>
2025-01-13 17:25:27 -0600

    Completed Exercise 1

--- app.js
+++ app.js
@@ -2,6 +2,28 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+function printOdds(count) {
+    if (count < 0) {
+        for (let i = 0; i >= count; i--) {
+            if (i % 2 != 0) {
+                console.log(i);
+            }
+        }
+    }
+    else if (count > 0) {
+        for (let i = 0; i <= count; i++) {
+            if (i % 2 != 0) {
+                console.log(i);
+            }
+        }
+    }
+    else {
+        console.log("Nothing to count.")
+    }
+}
 
+let count = 1;
+printOdds(count);
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
