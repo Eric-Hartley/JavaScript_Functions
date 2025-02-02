fa67b74e1dbfd5d42500874077feff265ebbdede
Eric Hartley <spiffydude0@gmail.com>
2025-02-02 12:26:30 -0600

    Updated code and logics to complete the exercises

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
 function printOdds(count) {
+
     if (count < 0) {
         for (let i = 0; i >= count; i--) {
             if (i % 2 != 0) {
@@ -18,35 +20,34 @@ function printOdds(count) {
         }
     }
     else {
-        console.log("Nothing to count.")
+        console.log(0)
     }
 }
+//let count = 10;
+//printOdds(count);
 
-let count = 1;
-printOdds(count);
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
-function checkAge(userAge, legalAge) {
+function checkAge(userAge) {
 
 
-    if (userAge >= legalAge) {
-        let aboveSixteen = console.log(`Congrats ${userName}, you can drive!`);
-    } else if (userAge < legalAge) {
-        let belowSixteen = console.log(`Sorry ${userName}, but you need to wait until you're 16.`);
+    if (userAge >= 16) {
+        console.log(`Congrats ${userName}, you can drive!`);
+    } else if (userAge < 16) {
+        console.log(`Sorry ${userName}, but you need to wait until you're 16.`);
     } else {
         console.log("Must argue (userAge, legalAge)")
     }
 
 }
 let userName = "Eric";
-let userAge = 15;
-const legalAge = 16;
-checkAge();
+let userAge = 16;
+checkAge(userAge);
+
 // Exercise 3 Section
 console.log("EXERCISE 3:\n==========\n");
 let x = 0;
 let y = 0;
-let result;
 if (x > 0 && y > 0) {
     result = console.log(`(${x}, ${y}) is in Quadrant 1`)
 } else if (x < 0 && y > 0) {
@@ -65,8 +66,9 @@ else if (x > 0 && y < 0) {
     result = console.log(`(${x}, ${y}) is the Origin`)
 }
 else {
-    result = console.log("Not in the Cartesian Plane.");
+    console.log("Not in the Cartesian Plane.");
 }
+
 // Exercise 4 Section
 console.log("EXERCISE 4:\n==========\n");
 let sideA = 5;
